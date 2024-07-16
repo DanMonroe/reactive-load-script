52fddafd44d91f0a7dfa34cf756c913eddf47ea8
Dan Monroe <dan.monroe@addepar.com>
2024-07-16 12:46:23 -0400

    Fix linting - removed hbs linting

--- reactive-load-script/src/addon-test-support/setup-load-script.ts
+++ reactive-load-script/src/addon-test-support/setup-load-script.ts
@@ -1,41 +1,38 @@
-import { getContext, settled, type TestContext } from '@ember/test-helpers';
-import LoadScriptService, { Script } from 'reactive-load-script/services/load-script';
+import { getContext, type TestContext } from '@ember/test-helpers';
+import LoadScriptService, {
+  Script,
+} from 'reactive-load-script/services/load-script';
 import type { PlainObject } from 'reactive-load-script';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import type NestedHooks from 'ember-qunit';
-
 let loadScriptService: LoadScriptService | undefined;
-// let injectScriptOriginal: (args: InjectScriptArgs) => void;
-// let getScriptOriginal: (src: string) => Script | undefined;
 
-export function setupLoadScript(hooks: NestedHooks, setupLoadScriptOptions: PlainObject = {}): void {
+export function setupLoadScript(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  hooks: any,
+  setupLoadScriptOptions: PlainObject = {},
+): void {
   hooks.beforeEach(async function (this: TestContext) {
     const { owner } = getContext() as TestContext;
 
-    loadScriptService = owner.lookup('service:load-script') as LoadScriptService;
+    loadScriptService = owner.lookup(
+      'service:load-script',
+    ) as LoadScriptService;
 
     loadScriptService.injectScript = () => {};
 
-    // loading: false,
-    // error: null,
-    // scriptLoaded: true,
-
     loadScriptService.getScript = (src) => {
-      let getScriptOptions = {...{
-        src,
-      }, ...setupLoadScriptOptions};
+      const getScriptOptions = {
+        ...{
+          src,
+        },
+        ...setupLoadScriptOptions,
+      };
 
-      let fakeScript = new Script(getScriptOptions);
-
-      return fakeScript;
+      return new Script(getScriptOptions);
     };
   });
 
   hooks.afterEach(function () {
-    // loadScriptService.injectScript = injectScriptOriginal;
-    // loadScriptService.getScript = getScriptOriginal;
     loadScriptService = undefined;
   });
-
 }
